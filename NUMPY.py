982c67693ea68faab3aef6fd1e9b4308989320f4
charan0416 <charanbaru169@gmail.com>
2025-03-26 15:41:01 +0530

    numpy

---
+++ NUMPY.py
@@ -0,0 +1,132 @@
+import numpy as np
+from numpy.conftest import dtype
+
+
+# arr = np.array([[[1,2,5],[1,2,3],[4,5,6],[7,8,9]]])
+# print(arr.ndim)
+
+# print(arr.shape)
+
+
+
+import numpy as np
+import hypothesis
+import pytest
+
+# import numpy as np
+#
+# x = np.array([[[1, 2, 3], [3, 4, 5], [6, 7, 8]],
+#               [[1, 3, 4], [2, 6, 7], [1, 4, 7]]])
+#
+# print(x.shape)
+#
+# print(x.ndim)
+#
+# print(x.base)
+#
+# print(x.dtype)
+
+
+
+# matrix = np.array([[[1,2,3],[4,5,6]],[[1,2,3],[1,2,3]]])
+# print(matrix.shape)
+
+
+# zeros_arr = np.zeros((2, 3))
+# print(zeros_arr)
+
+
+# identity_arr = np.eye(4)
+# print(identity_arr)
+
+# range_arr = np.arange(10)
+# print(range_arr)
+
+# arr1 = np.array([1, 2, 3])
+# arr2 = np.array([4, 5, 6])
+#
+# sum_arr = arr1 + arr2
+# print(sum_arr)
+#
+#
+# mul_arr = arr1*arr2
+# print(mul_arr)
+#
+# div_arr = arr1 % arr2
+# print(div_arr)
+#
+# sq_arr = arr1**arr2
+# print(sq_arr)
+
+
+# mean_arr = arr2.mean()
+# std_arr = arr2.std()
+# print(mean_arr)
+# print(std_arr)
+
+# arr = np.array([[1, 2, 3], [4, 5, 6]])
+# result = arr + 10
+# print(result)
+#
+# mul = arr *200
+# print(mul)
+
+# arr1 = np.array([[1, 2]])
+# arr2 = np.array([[3, 4]])
+
+# vstack_arr = np.vstack((arr1, arr2))
+# print(vstack_arr)
+
+
+# hstack_arr = np.hstack((arr1, arr2))
+# print(hstack_arr)
+
+
+# arr = np.array([10, 20, 30, 40, 50])
+# indices = [0, 2, 4]
+# print(arr[indices])
+
+
+# arr = np.array([1, 2, 3, 4, 5])
+# print(arr[arr > 2])
+
+# matrix = np.array([[1, 2], [3, 4]])
+# det = np.linalg.det(matrix)
+# print(det)
+
+
+# inv_matrix = np.linalg.inv(matrix)
+# print(inv_matrix)
+
+# arr = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+# print(arr[1, 2])
+
+
+
+# arr = np.array([10, 20, 30, 40, 50])
+# print(arr[1:4])
+
+
+# arr = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+# print(arr[:2, :2])
+
+
+# arr2d = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+# print(arr2d[[0, 2], [1, 2]])
+
+
+# arr = np.array([10, 20, 30, 40, 50])
+# arr[2] = 100
+# print(arr)
+
+
+
+# arr[2] = arr[2]+1000
+# print(arr)
+
+
+
+# arr = np.array([10, 20, 30, 40, 50])
+# indices = np.where(arr > 25)
+# print(indices)
+
